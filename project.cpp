946f797df2e94d74a05f4bbac970980875efbde4
Rafael Oliveira <rafdev.x@gmail.com>
2021-12-27 22:56:50 +0000

    continue 'fun' dev in project 1
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -7,15 +7,17 @@ typedef struct item {
 	long long prev; //prev is the index of the previous item on the previous pile
 } item;
 
-// chama com fun(piles, piles.size() - 1, 0)
-long long fun(std::vector<std::vector<item>> piles, long long pile_i, long long start_i)
+// chama com fun(piles, piles.size() - 1, inf OU primeiro da ultima pile)
+long long fun(std::vector<std::vector<item>> piles, long long pile_i, long long last)
 {
 	long long sum = 0;
 	if (pile_i == 0) {
 		return 1;
 	}
-	for (int i = start_i; i < piles[pile_i].size(); i++) {
-		sum += fun(piles, pile_i - 1, piles[pile_i][i].prev);
+	for (int i = 0; i < piles[pile_i].size(); i++) {
+		if (piles[pile_i][i].value > last)
+			continue;
+		sum += fun(piles, pile_i - 1, piles[pile_i][i].value);
 	}
 	return sum;
 }
@@ -41,7 +43,7 @@ std::string problem1()
 		}
 	}
 	std::cout << piles.size() << std::endl;
-	std::cout << fun(piles, piles.size() - 1, 0) << std::endl;
+	std::cout << fun(piles, piles.size() - 1, piles.back()[0].value) << std::endl;
 	return "what";
 }
 
