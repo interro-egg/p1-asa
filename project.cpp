50ff0df9d10728f9ab849df2cd206b7c8b874327
Rafael Oliveira <rafdev.x@gmail.com>
2022-01-01 22:27:10 +0000

    start implement binary search in problem 1
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -1,10 +1,11 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
 typedef struct item {
 	long long value;
-	long long prev; //prev is the index of the previous item on the previous pile
+	long long prev; // prev is the index of the previous item on the previous pile
 } item;
 
 // call this with: fun(piles, piles.size() - 1, piles.back().front())
@@ -28,23 +29,37 @@ long long fun(std::vector<std::vector<item>> piles, long long pile_i, item calle
 std::string problem1()
 {
 	std::vector<std::vector<item>> piles;
+	std::vector<long long> lastItems;
 	char c;
 	int number;
+
 	while (std::cin >> number) {
-		bool placed = false;
-		for (int i = 0; i < piles.size(); i++) {
-			if (piles[i].back().value >= number) {
-				long long prev = i > 0 ? piles[i - 1].size() - 1 : -1;
-				piles[i].push_back({number, prev});
-				placed = true;
-				break;
-			}
-		}
-		if (!placed) {
-			long long prev = piles.size() > 0 ? piles.back().size() - 1 : -1;
+
+		std::vector<long long>::iterator up = std::upper_bound(lastItems.begin(), lastItems.end(), number);
+		std::size_t i = up - lastItems.begin();
+		std::cout << "i for " << number << " is " << i << " (num piles = " << lastItems.size() << ")" << std::endl;
+		if (!lastItems.empty() && (up != lastItems.end() || lastItems.back() >= number)) {
+			// add to 'up' pile
+			long long prev = i > 0 ? piles[i - 1].size() - 1 : -1;
+			piles[i].push_back({number, prev});
+			lastItems[i] = number;
+		} else {
+			// create new pile
+			if (!lastItems.empty())
+				i++;
+			long long prev = i > 0 ? piles[i - 1].size() - 1 : -1;
 			piles.push_back({{number, prev}});
+			lastItems.push_back(number);
 		}
 	}
+	for (auto pile : piles) {
+		std::cout << "Pile: ";
+		for (auto item : pile) {
+			std::cout << item.value << " ";
+		}
+		std::cout << std::endl;
+	}
+
 	std::cout << piles.size() << std::endl;
 	std::cout << fun(piles, piles.size() - 1, piles.back().front()) << std::endl;
 	return "what";
