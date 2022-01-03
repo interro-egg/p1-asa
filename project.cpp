d70d738ff2fc94214f80c2fd8f57193a7dd422b8
Rafael Oliveira <rafdev.x@gmail.com>
2022-01-03 23:14:00 +0000

    start problem 2
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 std::string problem1()
 {
@@ -8,13 +10,46 @@ std::string problem1()
 
 std::string problem2()
 {
-	return "Not implemented\n";
+	std::vector<std::vector<long long>> matrix;
+	std::vector<long long> x, y;
+	std::string xStr, yStr;
+	long long number;
+
+	std::getline(std::cin, xStr);
+	std::getline(std::cin, yStr);
+	std::istringstream xSs(xStr);
+	std::istringstream ySs(yStr);
+
+	while (xSs >> number) {
+		x.push_back(number);
+	}
+
+	while (ySs >> number) {
+		y.push_back(number);
+	}
+
+	//! WRONG: does not care for INCREASING subsequences
+
+	for (int i = 0; i < x.size(); i++) {
+		for (int j = 0; j < y.size(); j++) {
+			if (i == 0 || j == 0) {
+				matrix[i][j] = 0;
+			} else if (x[i] == y[j]) {
+				matrix[i][j] = matrix[i - 1][j - 1] + 1;
+			} else {
+				matrix[i][j] = std::max(matrix[i - 1][j], matrix[i][j - 1]);
+			}
+		}
+	}
+
+	return "aa\n";
 }
 
 int main()
 {
 	int problem;
 	std::cin >> problem;
+	getchar(); // skip newline after problem number
 	switch (problem) {
 	case 1:
 		std::cout << problem1();
