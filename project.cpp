bea4eba6788e7c4f92891275a9ea38449eccc1ba
Rafael Oliveira <56204853+RafDevX@users.noreply.github.com>
2022-01-04 23:52:18 +0000

    Problem 1 (#4)
    * start project 1
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * continue 'fun' dev in project 1
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * 'fun' working
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * only keep track of caller on 'fun'
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * 'fun' actually not working
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * start implement binary search in problem 1
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * sort into piles working
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * finish problem 1
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * fix makefile to actually use compilation flags
    
    * fix warnings
    
    * remove duplicate newline
    
    * fix bug
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * pass vectors by reference
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * clarify item only refers to problem 1
    
    * fix segfault: don't access prev pile of first pile
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    * fix binary search ending for length-2 arrays
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>
    
    Co-authored-by: Diogo Cardoso <D-Card@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -1,12 +1,86 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
+typedef struct p1_item { // problem 1 item
+	long long value;
+	long long prev; // prev is the index of the previous item on the previous pile
+	long long sum;
+} p1_item;
+
+// Returns the index (starting from the end) of the first item whose value is greater than or equal to newItem's value, or -1 if none is found
+long long reverse_binary_search(std::vector<p1_item> &pile, std::size_t start, std::size_t end, long long newNum)
+{
+	if (pile[start].value < newNum) {
+		return -1;
+	}
+	std::size_t mid = (start + end) / 2;
+	while (start < end) {
+		if (mid == start && end > start) {
+			if (pile[end].value >= newNum) {
+				return end;
+			} else {
+				return start;
+			}
+		}
+		if (pile[mid].value >= newNum) {
+			start = mid;
+		} else {
+			end = mid - 1;
+		}
+		mid = (start + end) / 2;
+	}
+	return start;
+}
+
+long long get_sum(std::vector<std::vector<p1_item>> &piles, std::size_t i, long long newNum)
+{
+	if (i == 0) {
+		return (piles.empty() ? 0 : piles[0].back().sum) + 1;
+	}
+	// vv wouldn't work because end() > begin() and can't compare item directly
+	// std::vector<long long>::iterator lo = std::lower_bound(piles[i].end(), piles[i].begin(), newItem);
+	long long lo_i = reverse_binary_search(piles[i - 1], 0, piles[i - 1].size() - 1, newNum);
+	long long gteSum = lo_i == -1 ? 0 : piles[i - 1][lo_i].sum;
+	long long topSum = i + 1 <= piles.size() ? piles[i].back().sum : 0;
+	return piles[i - 1].back().sum - gteSum + topSum;
+}
+
 std::string problem1()
 {
-	return "Not implemented\n";
+	std::vector<std::vector<p1_item>> piles;
+	std::vector<long long> lastItems;
+	long long number;
+
+	while (std::cin >> number) {
+		std::vector<long long>::iterator up = std::upper_bound(lastItems.begin(), lastItems.end(), number);
+		std::size_t i = up - lastItems.begin();
+		if (lastItems.empty() || (up == lastItems.end() && lastItems.back() < number)) { // number is bigger than all lastItems
+			// create new pile
+			long long prev = i > 0 ? piles[i - 1].size() - 1 : -1;
+			piles.push_back({{number, prev, get_sum(piles, i, number)}});
+			lastItems.push_back(number);
+		} else {
+			// add to 'up' pile
+			if (up == lastItems.end() || (i > 0 && lastItems[i - 1] == number)) // number is equal to lastItems.back() (previous if statement failed so this is the only option)
+				i--;															// upper_bound returns the first element that is greater than number, so we need to check if previous pile's top is equal than number
+			long long prev = i > 0 ? piles[i - 1].size() - 1 : -1;
+			piles[i].push_back({number, prev, get_sum(piles, i, number)});
+			lastItems[i] = number;
+		}
+	}
+	// for (auto pile : piles) {
+	// 	std::cout << "Pile: ";
+	// 	for (auto p1_item : pile) {
+	// 		std::cout << "[ " << p1_item.value << " (sum=" << p1_item.sum << ")] ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	return std::to_string(piles.size()) + ' ' + std::to_string(piles.back().back().sum) + '\n';
 }
 
 std::string problem2()
